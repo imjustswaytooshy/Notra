51af93e6db5f13b7e20f987291270fdc385e043e
prism <snawalt420@proton.me>
2025-01-27 05:54:07 +0100

    minor changes

--- src/main/java/net/prism/notra/Notra.java
+++ src/main/java/net/prism/notra/Notra.java
@@ -1,19 +1,17 @@
 package net.prism.notra;
 
 import net.fabricmc.api.ModInitializer;
-
 import net.prism.notra.config.NotraConfig;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class Notra implements ModInitializer {
 	public static final String MOD_ID = "notra";
-
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
 	public void onInitialize() {
 		NotraConfig.getInstance();
-		LOGGER.info("NotraConfig oInitialized");
+		LOGGER.info("Notra mod initialized.");
 	}
 }
